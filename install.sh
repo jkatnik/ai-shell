c499be775c73e3aff51b7af8c2ea3391f2c418fb
Jarosław Kątnik <jaroslaw.katnik_ext@scout24.com>
2023-01-14 08:21:20 +0100

    improve installation script

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 npm install
-npm link --no-save
+#npm link --no-save
+npx link .
 
 # check if "ai" function exists, create it if it doesn't
 if [ -z "$(type -t ai)" ]; then
     # create foo function and save it to ~/.bash_aliases
     cat './src/ai-function.txt' >> ~/.bash_aliases
     source ~/.bash_aliases
+    echo "Reload bash aliases with: source ~/.bash_aliases"
 fi
