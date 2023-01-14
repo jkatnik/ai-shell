f48145c6d680f1d1c61575d1f557927fe310cf81
wiacekp.p <wiacekp.p@gmail.com>
2023-01-14 20:26:55 +0100

    reverted changes

--- install.sh
+++ install.sh
@@ -7,8 +7,7 @@ npx link .
 # check if "ai" function exists, create it if it doesn't
 if [ -z "$(type -t ai)" ]; then
     # create foo function and save it to ~/.bash_aliases
-    cat './src/ai-function.txt' >> ~/.bashrc
-    echo "alias ai-autocomplete='node ~/ai/dist/ai.js'" >> ~/.bashrc
-    source ~/.bashrc
+    cat './src/ai-function.txt' >> ~/.bash_aliases
+    source ~/.bash_aliases
     echo "Reload bash aliases with: source ~/.bash_aliases"
 fi
